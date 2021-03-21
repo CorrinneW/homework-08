4cec23c4b05539ebd1b1db8a1658edb4cfd9a228
Corrinne W <corrinneworden@gmail.com>
2021-03-20 21:09:14 -0400

    added responsive font size classes and header animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+//add Bulma font-sizng-classes
+$('h1').addClass('is-size-1-desktop is-size-2-tablet is-size-3-mobile');
+$('h2').addClass('is-size-3-desktop is-size-4-tablet is-size-5-mobile');
+$('h3').addClass('is-size-3-desktop is-size-4-tablet is-size-5-mobile');
+$('a').addClass('is-size-4-tablet is-size-5-mobile');
+$('p').addClass('is-size-5-tablet is-size-6-mobile');
+$('.social-icon').addClass('is-size-2-desktop is-size-3-tablet is-size-4-mobile')
+
 //controls function of mobile burger menu as well as the email contact form
 $(document).ready(function () {
     // Check for click events on the navbar burger icon
